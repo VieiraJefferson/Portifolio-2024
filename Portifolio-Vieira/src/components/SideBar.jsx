4f283cc4e2866b0bea97816bd7b8f339efd68fba
Jeff Vieira <myjeff22vieira@gmail.com>
2024-10-04 13:02:16 +0100

    finalização sidebar

--- Portifolio-Vieira/src/components/SideBar.jsx
+++ Portifolio-Vieira/src/components/SideBar.jsx
@@ -2,18 +2,27 @@ import SocialNetworks from "./SocialNetworks"
 import Avatar from "../img/eu1.jpg";
 import "../styles/components/sidebar.sass";
 import InformationContainer from "./InformationContainer";
+import curriculo from "./files/Curriculo.pdf";
+
 
 
 
 const SideBar = () => {
+
+
+
   return <aside id="sidebar">
       <img src={Avatar} alt="Jefferson Vieira" />
       <p className="title">Desenvolvedor Full Stack</p>
       <SocialNetworks />
       <InformationContainer />
-      <a href="" className="btn">
+      {/* <a href="" className="btn-download">
         Download currículo
-        </a>
+        </a>   */}
+        <button className="btn-download">
+          <a className="text-download" href={curriculo} download="curriculo">Descarregar Curriculo</a>
+        </button>
+      
   </aside>
 }
 
