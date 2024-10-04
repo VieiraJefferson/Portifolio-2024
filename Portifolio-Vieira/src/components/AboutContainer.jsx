71a806baf8eed16b086fb8992220975883197173
Jeff Vieira <myjeff22vieira@gmail.com>
2024-10-04 16:59:39 +0100

    personalização tech container

--- Portifolio-Vieira/src/components/AboutContainer.jsx
+++ Portifolio-Vieira/src/components/AboutContainer.jsx
@@ -1,8 +1,11 @@
 const AboutContainer = () => {
   return <section className="about-container">
     <h2>Sobre</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur provident magnam inventore voluptate libero veniam, deleniti quisquam consequatur labore animi dicta nostrum quas. Sit non nulla repellendus reiciendis veniam consequuntur.</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia at maiores rerum quo dolor blanditiis aperiam odit omnis totam quam animi quaerat vitae laborum voluptate, laboriosam officiis harum praesentium eius.</p>
+    <p>Minha experiência com á area de tecnologia vem do ensino médio técnico em informática e das atividades e projetos desenvolvidos em grupo e individualmente no bootcamp Labenu.
+       Utilizando as tecnologias HTML, CSS3, JavaScript, 
+      React,Bootstrap,StyledComponents, Chakra-UI, Material-UI, Trello, Postman, Node.Js, Github.
+       Venho retornando á área de tecnologia depois de passar por vasta experiência na área de vendas e posteriormente hotelaria.</p>
+  
 
   </section>
 }
--- Portifolio-Vieira/src/components/TechnologiesContainer.jsx
+++ Portifolio-Vieira/src/components/TechnologiesContainer.jsx
@@ -9,12 +9,12 @@ import {
 import "../styles/components/technologiescontainer.sass";
 
 const technologies = [
-  { id: "html", name: "HTML5", icon: <DiHtml5 /> },
-  { id: "css", name: "CSS3", icon: <DiCss3 /> },
-  { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
-  { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
-  { id: "mysql", name: "MySQL", icon: <DiMysql /> },
-  { id: "react", name: "React", icon: <DiReact /> },
+  { id: "html", name: "HTML5", resumo:"Linguagem de marcação pra estrutura de código e conteúdo web", icon: <DiHtml5 /> },
+  { id: "css", name: "CSS3",resumo:"Linguagem de regras de estilo, definição de cores fundo fontes e tudo relacionado à estilização do conteúdo web" ,icon: <DiCss3 /> },
+  { id: "js", name: "JavaScript",resumo:"Linguagem de Programação" , icon: <DiJsBadge /> },
+  { id: "node", name: "Node.js",resumo:"APIs, servidores web, aplicações em tempo real e utilitários de linha de comando" , icon: <DiNodejsSmall /> },
+  { id: "mysql", name: "MySQL",resumo:"Gerenciamento de banco de dados" , icon: <DiMysql /> },
+  { id: "react", name: "React",resumo:"Componentização, Renderizar condiçoes e listas, Eventos, Compartilhar dados entre componentes, JSX, Hooks, " , icon: <DiReact /> },
 ];
 
 
@@ -28,8 +28,7 @@ const TechnologiesContainer = () => {
             {tech.icon}
             <div className="technology-info">
               <h3>{tech.name} </h3>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores nobis tenetur pariatur
-                 dolorum veniam error sequi id excepturi repellendus,
+              <p>{tech.resumo}
                  </p>
             </div>
           </div>
